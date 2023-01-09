e014e1d1bd518375d0fa73dc657ca92e64ce3019
taejong12 <117630566+taejong12@users.noreply.github.com>
2023-01-09 09:44:28 +0900

    2023 01 09 월
    정리중 SHOP 관련된 파일 빼고 다 삭제

--- SpringPrac/FinalProject4/src/main/java/com/shop/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.shop.service;
-
-import com.shop.dto.JoinDTO;
-
-public interface UserService {
-	void join(JoinDTO join);
-	int overlapCheck(String value, String valueType);
-	void modifyInfo(String username, String valueType, String value);
-}
